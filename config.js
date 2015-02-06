fc92b12b645ef063016ad0e951e84ff37cc284ef
Grant Miiller <grant@m2iller.com>
2015-02-05 21:34:47 -0500

    Updated port for dev

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '127.0.0.1',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: '2368'
+            port: '3030'
         },
         paths: {
             contentPath: path.join(__dirname, '/content/')
